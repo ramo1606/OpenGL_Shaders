8b1a7289ec5f3ac1a5313cb4e3607469d38295a9
Ramo <ramo1606@protonmail.com>
2022-09-26 09:34:36 +0200

    first shader program

--- OpenGL_Projects/Chapter_2/src/main.cpp
+++ OpenGL_Projects/Chapter_2/src/main.cpp
@@ -4,21 +4,66 @@
 
 using namespace std;
 
-void init(GLFWwindow* window) { }
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() 
+{
+	const char* vShaderSource = "#version 430 \n"
+								"void main(void) \n"
+								"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+	const char* fShaderSource = "#version 430 \n"
+								"out vec4 color; \n"
+								"void main(void) \n"
+								"{ color = vec4(1.0, 1.0, 0.0, 1.0); }";
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vShaderSource, NULL);
+	glShaderSource(fShader, 1, &fShaderSource, NULL);
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) 
+{
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
 
 void display(GLFWwindow* window, double currentTime) {
-	glClearColor(1.0, 0.0, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(renderingProgram);
+	glPointSize(20.0f);
+	glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(void) {
+	//Initialize GLFW library
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	
+	//Set OpenGL version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 1", NULL, NULL);
+
+	//Create GLFW Window and openGL context
+	//Window of size 1280x1024, with title "Chapter2 - program 1", fullscreen and resource sharing are NULL
+	GLFWwindow* window = glfwCreateWindow(1280, 1024, "Chapter 2 - program 1", NULL, NULL);
 	glfwMakeContextCurrent(window);
+
 	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
-	glfwSwapInterval(1);
+	glfwSwapInterval(1);								//Enable VSync
 
 	init(window);
 
